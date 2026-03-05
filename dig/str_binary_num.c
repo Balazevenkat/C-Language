8336bcef5c55116d9c0b2c1a8fe487885852a551
BALAZEVENKAT <137323932+Balazevenkat@users.noreply.github.com>
2026-03-05 18:07:12 +0530

    Update str_binary_num.c

--- dig/str_binary_num.c
+++ dig/str_binary_num.c
@@ -1,3 +1,32 @@
+/*#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+    char str[50];
+    char *ptr=str;
+    fgets(ptr,50,stdin);
+    ptr[strlen(ptr)-1]='\0';
+    
+    //ptr=str[strlen(ptr)-1];
+    ptr = ptr + strlen(ptr) - 1;
+    int base=1,res=0;
+    while(*ptr)
+    {
+        if(*ptr=='0' || *ptr=='1')
+        {res+=base * (*ptr-'0');
+          base*=2;
+        }
+        else
+        break;
+        
+        ptr--;
+    }
+    
+    printf("res: %d",res);
+}*/
+
+
 #include<stdio.h>
 #include<string.h>
 
@@ -21,6 +50,19 @@ int main()
 
         p++;   // ✅ valid
     }
+    /*   ptr = ptr + strlen(ptr) - 1;
+    int base=1,res=0;
+    while(*ptr)
+    {
+        if(*ptr=='0' || *ptr=='1')
+        {res+=base * (*ptr-'0');
+          base*=2;
+        }
+        else
+        break;
+        
+        ptr--;
+    }*/
 
     printf("res: %d", res);
 }
