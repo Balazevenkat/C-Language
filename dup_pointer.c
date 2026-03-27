f1c14ad11bc011ef650f53778f54baf9f2addbf4
BALAZEVENKAT <137323932+Balazevenkat@users.noreply.github.com>
2026-03-27 21:26:37 +0530

    Create dup_pointer.c

---
+++ dup_pointer.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+    int flag;
+    char *str;
+    str=(char*)malloc(100*sizeof(char));
+    printf("Enter the string: ");
+    fgets(str,100,stdin);
+    str[strlen(str)-1]='\0';
+    char *start=str;
+    while(*start)
+    {
+        char *ptr=start+1;
+        while(*ptr)
+        {
+            if(*start == *ptr){
+            memmove(ptr,ptr+1,strlen(ptr+1)+1);
+            --ptr;
+            flag=1;
+            }
+            ptr++;
+        }
+        start++;
+    }
+    if(flag!=1)
+    {
+        printf("No duplicates");
+    }
+    else
+    puts(str);
+}
