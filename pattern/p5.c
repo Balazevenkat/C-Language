5cc26a865c9e05440bf3c93089fc30096f7c5b4b
BALAZEVENKAT <137323932+Balazevenkat@users.noreply.github.com>
2026-04-01 21:15:46 +0530

    Create p5.c

---
+++ pattern/p5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    for(int i=5;i>0;i--,printf("\n"))
+    {
+        for(int j=0;j<i;j++)
+        printf("*");
+    }
+}
+
+
+/*
+*****
+****
+***
+**
+*
+*/
