f5ccaf037fd9bf297d2c92ae725353ea8cab408c
BALAZEVENKAT <137323932+Balazevenkat@users.noreply.github.com>
2026-04-01 21:18:07 +0530

    Create p6.c

---
+++ pattern/p6.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int cnt=1;
+    for(int i=5;i>0;i--,printf("\n"))
+    {
+        for(int j=0;j<i;j++)
+        printf(" ");
+        
+        for(int k=0;k<cnt;k++)
+        printf("*");
+        
+        cnt++;
+    }
+}
+
+/*
+     *
+    **
+   ***
+  ****
+ *****
+
+  */
