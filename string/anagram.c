b13fb79c39e028b8daf010f38fd3b625a0f6f539
BALAZEVENKAT <137323932+Balazevenkat@users.noreply.github.com>
2026-04-04 16:00:37 +0530

    Create anagram.c

---
+++ string/anagram.c
@@ -0,0 +1,78 @@
+bbb
+abb
+{
+  int count[256] = {0};
+  /*
+  This creates an array of size 256 (for all ASCII characters).
+Every index represents a character.
+All values are initially 0.
+  */
+
+for (int i = 0; s1[i]; i++)
+    count[(unsigned char)s1[i]]++;
+
+for (int i = 0; s2[i]; i++)
+    count[(unsigned char)s2[i]]--;
+
+for (int i = 0; i < 256; i++)
+{
+    if (count[i] != 0)
+    {
+        printf("Not anagram");
+        return;
+    }
+}
+
+printf("anagram");
+}
+
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void check_anagram(char *str1,char*str2)
+{
+    char *s1=str1,*s2=str2;//always pointer with tem pointer don't use original pointer directly for irriration
+    int flag=0;
+    while(*s1)
+    {
+        s2=str2;
+        while(*s2)
+        {
+            if(*s1 == *s2)
+            flag=1;
+            
+            s2++;
+        }
+        if(flag==0)
+        break;
+        
+        s1++;
+    }
+    
+    if(flag)
+    printf("anagram");
+    else
+    printf("Not anagram");
+    
+}
+
+int main()
+{
+    char str1[100],str2[100];
+    fgets(str1,100,stdin);
+    str1[strlen(str1)-1]='\0';
+    
+    fgets(str2,100,stdin);
+    str2[strlen(str2)-1]='\0';
+
+    if(strlen(str1)!=strlen(str2))
+    {
+        printf("Not anagram...");
+        return 0;
+    }
+    
+    check_anagram(str1,str2);
+}
