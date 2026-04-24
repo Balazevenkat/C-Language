40a282c895ff1000603beb3e0adbd9d26f9e2de4
BALAZEVENKAT <137323932+Balazevenkat@users.noreply.github.com>
2026-04-24 17:57:50 +0530

    Update extra_space.c

--- string/extra_space.c
+++ string/extra_space.c
@@ -32,3 +32,36 @@ int main()
     
     puts(str);
 }
+
+
+/*#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+    char str[100];
+    fgets(str,100,stdin);
+    str[strlen(str)-1]='\0';
+    
+    char*ptr=str;
+
+   
+    while(*ptr)
+    {
+        if(*ptr==' ' && *(ptr+1)==' ')
+        {memmove(ptr,ptr+1,strlen(ptr+1)+1);
+        ptr--;
+        }
+        ptr++;
+    }
+    
+    if(strlen(str)==1)
+    {
+    printf("empty string");
+    return 0;
+    }
+    
+    
+    puts(str);
+}*/
